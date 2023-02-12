d385d0bf1a614289ab9ea3c5fd84a19c395ee57d
MINJEONG KIM <101111603+Mingguriguri@users.noreply.github.com>
2023-02-13 01:59:58 +0900

    Delete minjeong directory

--- minjeong/mysite/polls/static/polls/simple.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var hw = document.getElementById('hw');
-    hw.addEventListener('click', function(){
-        alert('Hello, friends! Please select a question.');
-    })
-}
